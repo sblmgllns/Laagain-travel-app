69ca91a64e1864bb1ccbf652587a6de6374256b9
sblmgllns <isabela.lobitana@gmail.com>
2025-05-28 21:23:26 +0800

    deployment diagnostic

--- src/supabase.js
+++ src/supabase.js
@@ -5,5 +5,6 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_SERVICE_ROLE_KEY 
+console.log("SUPABASE KEY:", import.meta.env.VITE_SUPABASE_ANON_KEY);
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
